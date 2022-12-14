1e3f087e28520d2e375307d672cff75fb8d812b9
Daria Lobanova <darialobanova911@gmail.com>
2022-12-14 03:42:24 +0300

    Add helper

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -5,7 +5,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | Microblog"
+    assert_select "title", "Microblog"
   end
 
   test "should get help" do
