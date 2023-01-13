6810c8318b2db82799e1a287028be1356a06a7a6
Daria Lobanova <darialobanova911@gmail.com>
2023-01-13 23:37:14 +0300

    Add sessions controller, helper's methods for log in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,10 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])         # if statemet is true only IF a user with the given email exists in the database
-                                                                      # AND (&&) has the given password
+    if user && user.authenticate(params[:session][:password])                   # if statemet is true only IF a user with the given email exists in the database
+ # params hash where params[:session][:email] and params[:session][:password]   # AND (&&) has the given password
+      log_in user
+      redirect_to user
     else
     flash.now[:danger] = "Invalid email or password"
     render 'new'
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,12 @@
 module SessionsHelper
+    # вход пользователя
+    def log_in(user)
+        session[:user_id] = user.id
+    end
+    # возвращает текущего пользователя, если уже есть (не ищет каждый раз по id в базе)
+    def current_user
+        if session[:user_id]
+            @current_user ||= User.find_by(id: session[:user_id])
+        end
+    end     
 end
