80aa2cd566f8610a91db7b28babc5f6c35062aeb
Daria Lobanova <darialobanova911@gmail.com>
2023-01-14 04:28:51 +0300

    Update User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    attr_accessor :remember_token
     before_save { email.downcase! }
     validates :name, presence: true, length: { maximum: 50 }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
@@ -7,16 +8,23 @@ class User < ApplicationRecord
                         uniqueness: { case_sensitive: false }
     has_secure_password
     validates :password, presence: true, length: { minimum: 8 }   
-    
 
+ # we can use self methods through class << self   
 
+    def User.digest(string) # = self.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
 
+# random token    
+    def User.new_token # = self.new_token
+        SecureRandom.urlsafe_base64    
+    end
 
-
-
-    # def User.digest(string)
-    # cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST
-    #                                               BCrypt::Engine.cost
-    # BCrypt::Password.create(string, cost: cost)
+# remembering the user for a persistent session    
+    def remember
+        self.remember_token = User.new_token
+        update_attribute(:remember_digest, User.digest(remember_token))
+    end    
 
 end
