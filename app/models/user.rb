4a6f77a1012527e0494377e9da789ea9d07813ec
Daria Lobanova <darialobanova911@gmail.com>
2023-01-15 06:13:43 +0300

    Update logout

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,6 +30,11 @@ class User < ApplicationRecord
 
 # return TRUE IF the given token matches the user's digest
     def authenticated?(remember_token)
+        return false if remember_digest.nil?
         BCrypt::Password.new(remember_digest).is_password?(remember_token)
     end
+# forgets a user    
+    def forget
+        update_attribute(:remember_digest, nil)
+    end    
 end
