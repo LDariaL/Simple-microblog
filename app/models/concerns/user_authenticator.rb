7128dbaf824e4e12c9d8a05c9125cb4499fee18f
Daria Lobanova <darialobanova911@gmail.com>
2023-02-05 16:55:33 +0300

    To plug auth module

--- app/models/concerns/user_authenticator.rb
+++
@@ -1,8 +0,0 @@
-
-# return TRUE IF the given token matches the user's digest
-  def authenticated?(remember_token)
-      return false if remember_digest.nil?
-      BCrypt::Password.new(remember_digest).is_password?(remember_token)
-  end
-
-end  
--- app/services/users/remember.rb
+++ app/services/users/remember.rb
@@ -1,13 +1,15 @@
 module Users
-  class Remember
+  class Remember 
+    
+    include ModuleAuth
 
     def initialize(user)
       @user = user
     end
     
     def call
-      if user
-        @user.remember_token = @user.generate_new_token
+      if @user
+        @user.remember_token = generate_new_token
         @user.remember_digest = user_digest(@user.remember_token)
         @user.save
         @user
@@ -16,22 +18,5 @@ module Users
       end   
     end  
 
-    private
-
-     # return TRUE IF the given token matches the user's digest
-    def authenticated?(remember_token)
-      return false if remember_digest.nil?
-      BCrypt::Password.new(remember_digest).is_password?(remember_token)
-    end
-    
-    def generate_new_token                  
-      SecureRandom.urlsafe_base64    
-    end
-
-    def user_digest(string)             
-      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                    BCrypt::Engine.cost
-      BCrypt::Password.create(string, cost: cost)
-    end
   end
 end
